42ff2fda4730142937271ac8fe3e94f0766841dc
AlexisHamon <alexis.hamon24@gmail.com>
2023-09-28 15:35:18 +0200

    🚑️ julia.c mallloc the image

--- TP/TP2-julia/julia/julia.c
+++ TP/TP2-julia/julia/julia.c
@@ -3,14 +3,22 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
+   if (argc < 2)
+    return 1;
+   int n = atoi(argv[1]);
+
+   if (n < 1)
+      return 0;
+   int width = 2 * n;
+   int height = n; 
+   
   FILE *out = fopen("outfile.bmp", "w");
   if (out == NULL) {
     perror("Cannot open outfile");
     exit(1);
   }
-  unsigned char *img = NULL;
+  unsigned char *img = malloc(width * height * sizeof(unsigned char));
 
-  // YOUR CODE HERE
   write_bmp_header(out, width, height);
   fwrite(img, width * height * 3, 1, out);
   fclose(out);
