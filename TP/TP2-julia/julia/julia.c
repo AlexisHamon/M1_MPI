c108adbb012acd4e41051649134decee32d987f0
AlexisHamon <alexis.hamon24@gmail.com>
2023-09-28 15:21:48 +0200

    clang-format provided code

--- TP/TP2-julia/julia/julia.c
+++ TP/TP2-julia/julia/julia.c
@@ -2,18 +2,18 @@
 #include <stddef.h>
 #include <stdlib.h>
 
-int main(int argc, char** argv) {
-   FILE *out=fopen("outfile.bmp","w");
-   if (out == NULL) {
-      perror("Cannot open outfile");
-      exit(1);
-   }
-   unsigned char* img = NULL;
-   
-   // YOUR CODE HERE
-   write_bmp_header(out, width, height);
-   fwrite(img, width*height*3, 1, out);
-   fclose(out);
-   
-   return 0;
+int main(int argc, char **argv) {
+  FILE *out = fopen("outfile.bmp", "w");
+  if (out == NULL) {
+    perror("Cannot open outfile");
+    exit(1);
+  }
+  unsigned char *img = NULL;
+
+  // YOUR CODE HERE
+  write_bmp_header(out, width, height);
+  fwrite(img, width * height * 3, 1, out);
+  fclose(out);
+
+  return 0;
 }
