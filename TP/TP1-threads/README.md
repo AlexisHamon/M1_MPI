53e04f16eec08310dd028d7d46e510ea37a7beb0
AlexisHamon <alexis.hamon24@gmail.com>
2023-09-28 03:45:02 +0200

    ✨ exercice 3

--- TP/TP1-threads/README.md
+++ TP/TP1-threads/README.md
@@ -20,3 +20,21 @@ En évitant `pthread_exit()`, élimine le psuedo-mem leak de valgrind (bug connu
 `atoi()` est préféré à `sscanf("%d", ...)` dans la lecture des arguments pour éviter de parser la chaîne de formattage.
 
 L'utilisation du mutex n'est nécessaire que lors de l'incrémentation de la somme partielle de chaque thread à la somme globale.
+
+## Exercice 3: Communication inter-threads
+
+Pour des raisons de simplification de passage d'arguments, c'est le `distributeur` qui va créer deux threads fils `chiffre` et `lettres`.
+
+On aurai bien pu déplacer le code de `distributeur` dans `main` et éviter la création d'un thread mais cela aurai eu pour effet de changer le skelette de code donné pour l'exercice.
+
+Pour la communication inter-threads, nous sommes dans un cas classique de producteur/consommateur dans lequel le producteur, ici `distributeur`, répartie les produits entre deux consommateurs, `chiffre` et `lettres`.
+
+L'idée va alors d'alouer un espace mémoire afin que le producteur puisse déposer ses produits qui seront consomés par le consommateur sans être bloquer.
+
+Si cela peut se faire avec une variable et deux mutex en attente passive, on en atteint vite les limitations. 
+
+On va donc opter pour une solution plus compliquée mais intéressante, l'allocation d'un espace mémoire circulaire `syncbuffer`. On utilise alors des sémaphores plutôt que des mutex pour leur compteurs.
+
+Le fait de disposer d'un seul producteur/consomateur par produit permet d'éviter de devoir poser des sections critiques sur les têtes d'écriture et de lecture du tableau circulaire.
+
+
--- TP/TP1-threads/distributeur.c
+++ TP/TP1-threads/distributeur.c
@@ -18,77 +18,143 @@
 
 #include <assert.h>
 #include <ctype.h>
+#include <pthread.h>
+#include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
+typedef struct syncbuffer {
+  unsigned length;
+  sem_t s_filled;
+  sem_t s_empty;
+  unsigned r_head;
+  unsigned w_head;
+  char *buffer;
+} syncbuffer;
+
+syncbuffer *syncbufferAlloc(unsigned value) {
+  syncbuffer *sbuf = malloc(sizeof(syncbuffer));
+  sbuf->length = value;
+  sem_init(&sbuf->s_filled, 0, 0);
+  sem_init(&sbuf->s_empty, 0, value);
+  sbuf->r_head = 0;
+  sbuf->w_head = 0;
+  sbuf->buffer = malloc(value * sizeof(char));
+  return sbuf;
+}
+
+void syncbufferFree(syncbuffer *sbuf) {
+  sem_destroy(&sbuf->s_filled);
+  sem_destroy(&sbuf->s_empty);
+  free(sbuf->buffer);
+  free(sbuf);
+}
+
+void syncbufferAppend(syncbuffer *sbuf, char car) {
+  sem_wait(&sbuf->s_empty);
+  sbuf->buffer[sbuf->r_head] = car;
+  sbuf->r_head = (sbuf->r_head + 1) % sbuf->length;
+  sem_post(&sbuf->s_filled);
+}
+
+char syncbufferGet(syncbuffer *sbuf) {
+  sem_wait(&sbuf->s_filled);
+  char car = sbuf->buffer[sbuf->w_head];
+  sbuf->w_head = (sbuf->w_head + 1) % sbuf->length;
+  sem_post(&sbuf->s_empty);
+  return car;
+}
+
 /*
 ** Somme des chiffres.
 */
-int chiffre(int car) {
-  static int somme = 0 ;
-  
-  if (car != EOF) {
-    /* Somme des chiffres. */
-    somme += (car - '0') ;
-  } else {
-    /* Renvoi du résultat. */
-    return somme ;
-  }
-  return 0 ;
+void *chiffre(void *ptr) {
+  syncbuffer *chiffre_buffer = (syncbuffer *)ptr;
+
+  static int somme = 0;
+  char car;
+
+  while ((car = syncbufferGet(chiffre_buffer)) != EOF) {
+    somme += (car - '0');
+  };
+
+  return (void *)&somme;
 }
 
 /*
 ** Fréquence des lettres.
 */
-int* lettre(int car) {
-  /* On suppose que le tableau est correctement initialisé. */
-  static int frequence['z' - 'a' + 1] ;
-  
-  if (car != EOF) {
-    /* Incrément des fréquences. */
-    frequence [ tolower ( car ) - 'a' ] ++ ;
-  } else {
-    /* Renvoi du résultat. */
-    return frequence ;
-  }
-  return NULL ;
+void *lettre(void *ptr) {
+  syncbuffer *lettre_buffer = (syncbuffer *)ptr;
+
+  static int frequence['z' - 'a' + 1];
+  char car;
+
+  while ((car = syncbufferGet(lettre_buffer)) != EOF) {
+    frequence[tolower(car) - 'a']++;
+  };
+
+  return (void *)frequence;
 }
 
 /*
 ** Distribution des caractères et affichage des résultats.
 */
-void distributeur() {
-  int car, *frequence, i, somme ;
-  
+void *distributeur(void *ignored) {
+  /* Création des Threads Concurrents */
+
+  syncbuffer *chiffre_buffer = syncbufferAlloc(512);
+  pthread_t pth_chiffre;
+  pthread_create(&pth_chiffre, NULL, chiffre, (void *)chiffre_buffer);
+
+  syncbuffer *lettre_buffer = syncbufferAlloc(512);
+  pthread_t pth_lettre;
+  pthread_create(&pth_lettre, NULL, lettre, (void *)lettre_buffer);
+
   /* Distribution des caractères. */
+  int car;
   do {
-    car = getchar() ;
+    car = getchar();
     if (isalpha(car) || (car == EOF)) {
-      frequence = lettre ( car ) ;
+      syncbufferAppend(lettre_buffer, car);
     }
     if (isdigit(car) || (car == EOF)) {
-      somme = chiffre(car);
+      syncbufferAppend(chiffre_buffer, car);
     }
-  } while(car != EOF);
-  
+  } while (car != EOF);
+
+  /* Attente des Threads Concurrents */
+  int *somme_ptr;
+  pthread_join(pth_chiffre, (void *)&somme_ptr);
+  syncbufferFree(chiffre_buffer);
+  int *frequence_ptr;
+  pthread_join(pth_lettre, (void *)&frequence_ptr);
+  syncbufferFree(lettre_buffer);
+
   /* Lecture et affichage de la somme. */
-  printf("Somme : %d", somme);
-  
+  printf("Somme : %d\n", *somme_ptr);
+
   /* Lecture et affichage des frequences. */
-  for(i = 'a' ; i <= 'z' ; i ++) {
-    printf(" ; %c : %d", i, frequence[i - 'a']) ;
+  for (int i = 'a'; i <= 'z'; i++) {
+    printf(" ; %c : %d", i, frequence_ptr[i - 'a']);
   }
   printf("\n");
+
+  return NULL;
 }
 
 /*
 ** Lancement de la fonction de distribution.
 */
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   /* Lancement du travail. */
-  distributeur();
-  
+  pthread_t pth_distrib;
+  pthread_create(&pth_distrib, NULL, distributeur, NULL);
+
+  /* Attente du travail. */
+  pthread_join(pth_distrib, NULL);
+
   /* Tout s'est bien terminé. */
   exit(0);
 }
